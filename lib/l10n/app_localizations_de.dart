02aa48ab38e8d4aceb8e830083f4171b036fbc1b
Mohamed Okaily <mosala7.me@gmail.com>
2025-06-15 03:51:50 +0200

    prepare language files

---
+++ lib/l10n/app_localizations_de.dart
@@ -0,0 +1,19 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for German (`de`).
+class AppLocalizationsDe extends AppLocalizations {
+  AppLocalizationsDe([String locale = 'de']) : super(locale);
+
+  @override
+  String get appTitle => 'Roadsurfer Demo';
+
+  @override
+  String get campsitesTitle => 'Campingplätze';
+
+  @override
+  String get detailsTitle => 'Details';
+}
