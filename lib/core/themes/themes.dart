2ed7e012e8a6bb7d72d2dc4811b0156193e29e3d
Mohamed Okaily <mosala7.me@gmail.com>
2025-06-15 22:55:45 +0200

    Update themes.dart

--- lib/core/themes/themes.dart
+++ lib/core/themes/themes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:roadsurferdemo/core/themes/theme_utils.dart';
 import 'color_palettes.dart';
 
 class Themes {
@@ -7,7 +8,7 @@ class Themes {
   Themes({required this.baseContext});
 
   static final lightTheme = ThemeData(
-    primarySwatch: Colors.green,
+    primarySwatch: createMaterialColor(ColorPalettes.primary),
     useMaterial3: false,
     fontFamily: 'EncodeSans',
     primaryColor: ColorPalettes.primary,
@@ -21,6 +22,30 @@ class Themes {
       displayLarge: TextStyle(fontWeight: FontWeight.w300, fontSize: 30, color: ColorPalettes.secondary),
       displayMedium: TextStyle(fontWeight: FontWeight.w700, fontSize: 26, color: ColorPalettes.secondary),
       displaySmall: TextStyle(fontWeight: FontWeight.w400, fontSize: 22, color: ColorPalettes.secondary),
+    ),
+    appBarTheme: AppBarTheme(
+      titleSpacing: 50,
+      backgroundColor: ColorPalettes.primary,
+      foregroundColor: ColorPalettes.secondary,
+      elevation: 0,
+      shadowColor: ColorPalettes.secondary.withValues(alpha: 120),
+      toolbarHeight: 60,
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      enabledBorder: InputBorder.none,
+      contentPadding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+      suffixIconColor: ColorPalettes.secondary,
+      focusColor: Colors.transparent,
+    ),
+    dropdownMenuTheme: DropdownMenuThemeData(
+      textStyle: TextStyle(
+        fontWeight: FontWeight.w500,
+        color: ColorPalettes.secondary,
+      ),
+      menuStyle: MenuStyle(
+        backgroundColor: WidgetStatePropertyAll(ColorPalettes.primary),
+        surfaceTintColor: const WidgetStatePropertyAll(Colors.white),
+      ),
     ),
   );
 
